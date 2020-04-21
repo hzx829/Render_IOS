268085cc95cef5d90ce996df83a6b5da7f992e6d
BiGy2018 <360055070@qq.com>
2020-04-21 17:55:29 +0800

    Add files via upload

---
+++ Render_IOS/Render_IOS/Render_IOS.h
@@ -0,0 +1,19 @@
+//
+//  Render_IOS.h
+//  Render_IOS
+//
+//  Created by 黄治宪 on 2020/4/21.
+//  Copyright © 2020 黄治宪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Render_IOS.
+FOUNDATION_EXPORT double Render_IOSVersionNumber;
+
+//! Project version string for Render_IOS.
+FOUNDATION_EXPORT const unsigned char Render_IOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Render_IOS/PublicHeader.h>
+
+
